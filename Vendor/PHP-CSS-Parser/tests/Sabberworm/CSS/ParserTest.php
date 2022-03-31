d03cc22d7c4c72eac444f8b4f316eed7ede07984
Franz Holzinger <franz.holzinger@festwein.de>
2022-03-31 21:55:01 +0200

    * PHP-CSS-Parser 8.3.0

--- Vendor/PHP-CSS-Parser/tests/Sabberworm/CSS/ParserTest.php
+++ Vendor/PHP-CSS-Parser/tests/Sabberworm/CSS/ParserTest.php
@@ -126,6 +126,12 @@ class ParserTest extends \PHPUnit_Framework_TestCase {
 		}
 	}
 
+	function testUnicodeRangeParsing() {
+		$oDoc = $this->parsedStructureForFile('unicode-range');
+		$sExpected = "@font-face {unicode-range: U+0100-024F,U+0259,U+1E??-2EFF,U+202F;}";
+		$this->assertSame($sExpected, $oDoc->render());
+	}
+
 	function testSpecificity() {
 		$oDoc = $this->parsedStructureForFile('specificity');
 		$oDeclarationBlock = $oDoc->getAllDeclarationBlocks();
@@ -387,10 +393,24 @@ body {background-url: url("http://somesite.com/images/someimage.gif");}';
 		$this->assertSame($sExpected, $oDoc->render());
 	}
 
+	function testHexAlphaInFile() {
+		$oDoc = $this->parsedStructureForFile('hex-alpha', Settings::create()->withMultibyteSupport(true));
+		$sExpected = 'div {background: rgba(17,34,51,.27);}
+div {background: rgba(17,34,51,.27);}';
+		$this->assertSame($sExpected, $oDoc->render());
+	}
+
 	function testCalcInFile() {
 		$oDoc = $this->parsedStructureForFile('calc', Settings::create()->withMultibyteSupport(true));
 		$sExpected = 'div {width: calc(100% / 4);}
+div {margin-top: calc(-120% - 4px);}
 div {height: -webkit-calc(9 / 16 * 100%) !important;width: -moz-calc(( 50px - 50% ) * 2);}';
+		$this->assertSame($sExpected, $oDoc->render());
+	}
+
+	function testCalcNestedInFile() {
+		$oDoc = $this->parsedStructureForFile('calc-nested', Settings::create()->withMultibyteSupport(true));
+		$sExpected = '.test {font-size: calc(( 3 * 4px ) + -2px);top: calc(200px - calc(20 * 3px));}';
 		$this->assertSame($sExpected, $oDoc->render());
 	}
 
@@ -403,6 +423,12 @@ div {height: -webkit-calc(9 / 16 * 100%) !important;width: -moz-calc(( 50px - 50
 	function testEmptyGridLineNameLenientInFile() {
 		$oDoc = $this->parsedStructureForFile('empty-grid-linename');
 		$sExpected = '.test {grid-template-columns: [] 100px;}';
+		$this->assertSame($sExpected, $oDoc->render());
+	}
+
+	function testUnmatchedBracesInFile() {
+		$oDoc = $this->parsedStructureForFile('unmatched_braces', Settings::create()->withMultibyteSupport(true));
+		$sExpected = 'button, input, checkbox, textarea {outline: 0;margin: 0;}';
 		$this->assertSame($sExpected, $oDoc->render());
 	}
 
@@ -458,26 +484,63 @@ body {background-url: url("http://somesite.com/images/someimage.gif");}';
 	}
 
 	/**
-	* @expectedException Sabberworm\CSS\Parsing\UnexpectedTokenException
-	*/
+	 * @expectedException \Sabberworm\CSS\Parsing\UnexpectedTokenException
+	 */
 	function testCharsetFailure1() {
 		$this->parsedStructureForFile('-charset-after-rule', Settings::create()->withLenientParsing(false));
 	}
 
 	/**
-	* @expectedException Sabberworm\CSS\Parsing\UnexpectedTokenException
-	*/
+	 * @expectedException \Sabberworm\CSS\Parsing\UnexpectedTokenException
+	 */
 	function testCharsetFailure2() {
 		$this->parsedStructureForFile('-charset-in-block', Settings::create()->withLenientParsing(false));
 	}
 
 	/**
-	* @expectedException Sabberworm\CSS\Parsing\SourceException
-	*/
+	 * @expectedException \Sabberworm\CSS\Parsing\SourceException
+	 */
 	function testUnopenedClosingBracketFailure() {
 		$this->parsedStructureForFile('unopened-close-brackets', Settings::create()->withLenientParsing(false));
 	}
 
+	/**
+	 * Ensure that a missing property value raises an exception.
+	 *
+	 * @expectedException \Sabberworm\CSS\Parsing\UnexpectedTokenException
+	 * @covers \Sabberworm\CSS\Value\Value::parseValue()
+	 */
+	function testMissingPropertyValueStrict() {
+		$this->parsedStructureForFile('missing-property-value', Settings::create()->withLenientParsing(false));
+	}
+
+	/**
+	 * Ensure that a missing property value is ignored when in lenient parsing mode.
+	 *
+	 * @covers \Sabberworm\CSS\Value\Value::parseValue()
+	 */
+	function testMissingPropertyValueLenient() {
+		$parsed = $this->parsedStructureForFile('missing-property-value', Settings::create()->withLenientParsing(true));
+		$rulesets = $parsed->getAllRuleSets();
+		$this->assertCount( 1, $rulesets );
+		$block = $rulesets[0];
+		$this->assertTrue( $block instanceof DeclarationBlock );
+		$this->assertEquals( array( 'div' ), $block->getSelectors() );
+		$rules = $block->getRules();
+		$this->assertCount( 1, $rules );
+		$rule = $rules[0];
+		$this->assertEquals( 'display', $rule->getRule() );
+		$this->assertEquals( 'inline-block', $rule->getValue() );
+	}
+
+	/**
+	 * Parse structure for file.
+	 *
+	 * @param string      $sFileName Filename.
+	 * @param null|obJeCt $oSettings Settings.
+	 *
+	 * @return CSSList\Document Parsed document.
+	 */
 	function parsedStructureForFile($sFileName, $oSettings = null) {
 		$sFile = dirname(__FILE__) . '/../../files' . DIRECTORY_SEPARATOR . "$sFileName.css";
 		$oParser = new Parser(file_get_contents($sFile), $oSettings);
